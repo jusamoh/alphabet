5d3de0aac0e7fdedf557f93fe8126a8167243655
jusamoh <152563308+jusamoh@users.noreply.github.com>
2025-01-04 15:42:38 +0900

    Add files via upload

--- sw.js
+++ sw.js
@@ -46,7 +46,7 @@ self.addEventListener('activate', event => {
 self.addEventListener('fetch', event => {
     event.respondWith(
         caches.match(event.request).then(response => {
-            return response || fetch(event.request).catch(() => caches.match('/offline.html'));
+            return response || fetch(event.request).catch(() => caches.match('/alphabet/offline.html'));
         })
     );
 });
